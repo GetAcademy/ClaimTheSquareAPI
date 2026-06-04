2cbb59ea02c75ffa5adc41793785e47cf07cb039
GET Academy <terje@kolderup.net>
2026-06-04 11:00:43 +0200

    x

--- ClaimTheSquareAPI/Program.cs
+++ ClaimTheSquareAPI/Program.cs
@@ -1,26 +1,61 @@
+using System.Text.Json;
 using ClaimTheSquareAPI.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
 
-var squares = new List<Square>
-{
-    new Square{Index = 5, Text = "Terje", BackColor = "Blue", ForeColor = "White"},
-    new Square{Index = 15, Text = "Per", BackColor = "White", ForeColor = "Green"},
-};
+var fileName = "squares.json";
 
 app.MapGet("/api/square", () =>
 {
-    return squares;
+    if (!File.Exists(fileName)) return Array.Empty<Square>();
+    var json = File.ReadAllText("squares.json");
+    return JsonSerializer.Deserialize<Square[]>(json);
 });
-app.MapPost("/api/square", () =>
+
+app.MapPost("/api/square", (Square square) =>
 {
-    return new
+    var squares = new List<Square>();
+    if (File.Exists(fileName))
     {
-        Terje = 5,
-    };
+        var json1 = File.ReadAllText(fileName);
+        var squaresArray = JsonSerializer.Deserialize<Square[]>(json1);
+        squares.AddRange(squaresArray);
+    }
+
+    squares.Add(square);
+    var json2 = JsonSerializer.Serialize(squares);
+    File.WriteAllText(fileName, json2);
 });
 app.UseStaticFiles();
 //app.UseDefaultFiles("index.html");
 app.Run();
+
+
+/*
+   using ClaimTheSquareAPI.DTOs;
+   
+   var builder = WebApplication.CreateBuilder(args);
+   var app = builder.Build();
+   app.UseHttpsRedirection();
+   
+   var squares = new List<Square>
+   {
+       new Square{Index = 50, Text = "Terje", BackColor = "Blue", ForeColor = "White"},
+       new Square{Index = 15, Text = "Per", BackColor = "White", ForeColor = "Green"},
+   };
+   
+   app.MapGet("/api/square", () =>
+   {
+       return squares;
+   });
+   app.MapPost("/api/square", (Square square) =>
+   {
+       squares.Add(square);
+   });
+   app.UseStaticFiles();
+   //app.UseDefaultFiles("index.html");
+   app.Run();
+    
+ */
