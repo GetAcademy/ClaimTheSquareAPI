597f01c3c97cacb2f02e30b77860e68f3c0932f3
GET Academy <terje@kolderup.net>
2026-06-04 11:22:24 +0200

    x

---
+++ ClaimTheSquareAPI/Infrastructure/SquareRepository.cs
@@ -0,0 +1,30 @@
+﻿using ClaimTheSquareAPI.DTOs;
+using System.Text.Json;
+
+namespace ClaimTheSquareAPI.Infrastructure
+{
+    public class SquareRepository
+    {
+        const string fileName = "squares.json";
+
+        public static List<Square> GetAll()
+        {
+            if (!File.Exists(fileName)) return new List<Square>();
+            var json = File.ReadAllText("squares.json");
+            return new List<Square>(JsonSerializer.Deserialize<Square[]>(json));
+        }
+
+        public static void AddSquare(Square square)
+        {
+            var squares = GetAll();
+            squares.Add(square);
+            WriteAllSquares(squares);
+        }
+
+        private static void WriteAllSquares(List<Square> squares)
+        {
+            var json2 = JsonSerializer.Serialize(squares);
+            File.WriteAllText(fileName, json2);
+        }
+    }
+}
--- ClaimTheSquareAPI/Program.cs
+++ ClaimTheSquareAPI/Program.cs
@@ -1,35 +1,12 @@
-using System.Text.Json;
-using ClaimTheSquareAPI.DTOs;
+using ClaimTheSquareAPI.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
 
-var fileName = "squares.json";
-
-app.MapGet("/api/square", () =>
-{
-    if (!File.Exists(fileName)) return Array.Empty<Square>();
-    var json = File.ReadAllText("squares.json");
-    return JsonSerializer.Deserialize<Square[]>(json);
-});
-
-app.MapPost("/api/square", (Square square) =>
-{
-    var squares = new List<Square>();
-    if (File.Exists(fileName))
-    {
-        var json1 = File.ReadAllText(fileName);
-        var squaresArray = JsonSerializer.Deserialize<Square[]>(json1);
-        squares.AddRange(squaresArray);
-    }
-
-    squares.Add(square);
-    var json2 = JsonSerializer.Serialize(squares);
-    File.WriteAllText(fileName, json2);
-});
+app.MapGet("/api/square", SquareRepository.GetAll);
+app.MapPost("/api/square", SquareRepository.AddSquare);
 app.UseStaticFiles();
-//app.UseDefaultFiles("index.html");
 app.Run();
 
 
@@ -37,25 +14,25 @@ app.Run();
    using ClaimTheSquareAPI.DTOs;
    
    var builder = WebApplication.CreateBuilder(args);
-   var app = builder.Build();
-   app.UseHttpsRedirection();
-   
-   var squares = new List<Square>
+var app = builder.Build();
+app.UseHttpsRedirection();
+
+var squares = new List<Square>
    {
        new Square{Index = 50, Text = "Terje", BackColor = "Blue", ForeColor = "White"},
        new Square{Index = 15, Text = "Per", BackColor = "White", ForeColor = "Green"},
    };
-   
-   app.MapGet("/api/square", () =>
-   {
-       return squares;
-   });
-   app.MapPost("/api/square", (Square square) =>
-   {
-       squares.Add(square);
-   });
-   app.UseStaticFiles();
-   //app.UseDefaultFiles("index.html");
-   app.Run();
-    
- */
+
+app.MapGet("/api/square", () =>
+{
+    return squares;
+});
+app.MapPost("/api/square", (Square square) =>
+{
+    squares.Add(square);
+});
+app.UseStaticFiles();
+//app.UseDefaultFiles("index.html");
+app.Run();
+
+*/
